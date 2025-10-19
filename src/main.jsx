49e02105900173a62d70ebaf80caf1993ffb34f6
Your Name <your_email@example.com>
2025-10-19 18:58:57 +0700

    DNT-91

--- src/main.jsx
+++ src/main.jsx
@@ -64,9 +64,8 @@ createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <StrictMode>
       <BrowserRouter>
-      <App />
         <PasswordGate>
-          
+          <App />
         </PasswordGate>
       </BrowserRouter>
     </StrictMode>
